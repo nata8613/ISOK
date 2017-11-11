a169ac5ad7ce9151f0428f42906d42c952826947
skilja95 <milivojeskiljevic7@gmail.com>
2017-11-11 19:01:52 +0100

    Inicijalizacija PayPal front-end
    Aplikacija povezana sa paypay checkout servisom. Podaci za slanje, za sada testni i inicijalni. Potrebno poslati realne podatke i preuzeti statuse o transakciji.

--- angular4-frontend/src/app/homeinsform/homeinsform.component.ts
+++ angular4-frontend/src/app/homeinsform/homeinsform.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare let paypal: any;
+
 @Component({
   selector: 'app-homeinsform',
   templateUrl: './homeinsform.component.html',
@@ -10,6 +12,38 @@ export class HomeinsformComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit(): void {
+   
+      paypal.Button.render({
+        env: 'sandbox',
+        
+
+
+        client: {
+          production: 'EGwY_-5zByiyP8Km2cTU4Wf70CHlwZr9AJqF9BIwLBf-xyDcMg6tbVGBPLo-NZTlP4pcgVBCCrDIdi1k',
+          sandbox: 'AQSA9iBWQSdWQWVGyusPw70GcuVoB91i-nVZCSCuW2QpFJa7-HWUT2ABimq3PMCnqOsOLIH1zREZEnl4'
+        },
+        commit: true,
+        payment: function (data, actions) {
+          return actions.payment.create({
+            payment: {
+              transactions: [
+                {
+                  amount: { total: '1.00', currency: 'USD' }
+                }
+              ]
+            }
+          })
+        },
+        onAuthorize: function(data, actions) {
+          return actions.payment.execute().then(function(payment) {
+            // TODO
+          })
+        }
+      }, '#paypal-button');
+   
   }
 
 }
