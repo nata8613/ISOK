6031d647dab95273c6a6806131290a89b2e2ce51
nata8613 <natasa.subic94@gmail.com>
2018-01-29 14:32:49 +0100

    Update VehicleInsurance.java

--- DC/dc/src/main/java/datacentar/dc/isok/model/VehicleInsurance.java
+++ DC/dc/src/main/java/datacentar/dc/isok/model/VehicleInsurance.java
@@ -34,7 +34,7 @@ public class VehicleInsurance {
 	@Column(nullable = false)
 	private String ownerSurname;
 	
-	@Column(nullable = false)
+	@Column(nullable = true)
 	private String jmbg;
 
 	public VehicleInsurance() {
